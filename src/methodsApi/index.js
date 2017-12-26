1a68a055e7de22b21c7ebdb3e1f11cb8e8f97bb7
WYseven <consolelog@126.com>
2017-12-26 23:51:34 +0800

    未解决跨域存cookie问题

--- src/methodsApi/index.js
+++ src/methodsApi/index.js
@@ -1,6 +1,8 @@
 
 import axios from 'axios'
 
+axios.defaults.withCredentials = true
+
 const baseUrl = 'http://localhost:3001/'
 
 const url = require('url');
@@ -9,8 +11,15 @@ function resolve(path){
   return url.resolve(baseUrl,path)
 }
 
+
+
 export  function loginMethod (data) {
-  return axios.post(resolve('/login'),data)
+  return axios(resolve('/login'),{
+    method: "post",
+    data: data,
+    credentials: 'same-origin',
+    withCredentials: true
+  })
 }
 export  function getUsersAll () {
   return axios.get(resolve('/usersAll'))
