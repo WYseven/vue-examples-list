119e2017af0d2786e899ebc69895862c90ab6b97
WYseven <consolelog@126.com>
2018-01-01 09:55:33 +0800

    修改cnnode布局

--- src/views/third-party/cnnode/index.vue
+++ src/views/third-party/cnnode/index.vue
@@ -6,19 +6,34 @@
       </MenuItem>
     </Menu>
     <div class="content">
-      <Row type="flex">
-          <Col span="12">
+      <Row>
+        <Col class="demo-spin-col" span="24">
+            <Spin fix>
+                <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
+                <div>Loading</div>
+            </Spin>
+        </Col>
+      </Row>
+      <Row class="custom" type="flex">
+          <Col span="3" class="one-col">
             <a href="">
               <img src="https://avatars3.githubusercontent.com/u/3118295?v=4&s=120" alt="">
             </a>
-            <span>100/1000</span> 
-            <span>置顶</span>
-            <a href="">测试请发到客户端测试专区，违规影响用户的，直接封号</a>
+            <span>100/100000</span> 
+            <span class="flag flag-share">置顶</span>
           </Col>
-          <Col span="3" offset="8">
-            <img src="https://avatars3.githubusercontent.com/u/20333833?v=4&s=120" alt="">
+          <Col span="12" offset="1">
+            <a href="" class="title">测试请发到客户端测试专区，违规影响用户的，直接封号</a>
+          </Col>
+          <Col span="3" offset="3">
+            <a href="">
+              <img src="https://avatars3.githubusercontent.com/u/20333833?v=4&s=120" alt="">
+            </a>
             <span>12小时前</span>
           </Col>
+      </Row>
+      <Row class="page">
+        <Page :total="1000" show-elevator></Page>
       </Row>
     </div>
   </div>
@@ -75,6 +90,26 @@ export default {
 }
 </script>
 <style scoped>
+.demo-spin-icon-load{
+    animation: ani-demo-spin 1s linear infinite;
+}
+@keyframes ani-demo-spin {
+    from { transform: rotate(0deg);}
+    50%  { transform: rotate(180deg);}
+    to   { transform: rotate(360deg);}
+}
+.demo-spin-col{
+    height: 100px;
+    position: relative;
+    border: 1px solid #eee;
+}
+a {
+  font-size: 16px;
+  color: #000;
+}
+.ivu-menu-item-selected a {
+  color: #2d8cf0;
+}
 .cnnode {
   width: 90%;
   margin: 0px auto;
@@ -88,13 +123,40 @@ export default {
   background: #fff;
   margin-top: 10px;
 }
+.custom {
+  padding: 10px 0;
+}
+.one-col span,.one-col a{
+  display: inline-block;
+}
 .content img {
   width: 30px;
   height: 30px;
   vertical-align: middle;
 }
-.content span {
+.content span, .content a{
   vertical-align: middle;
+  line-height: 2.5em;
+}
+.flag {
+  display: block;
+  height: 30px;
+  border-radius: 2px;
+  padding: 2px 4px;
+  text-align: center;
+  color: #fff;
+  font-size: 12px;
+  font-weight: bold;
+}
+.flag-top {
+  background: #80bd01;
+}
+.flag-share {
+  background: #e5e5e5;
+}
+.page {
+  border-top: 1px solid #ccc;
+  padding: 10px 10px;
 }
 </style>
 
